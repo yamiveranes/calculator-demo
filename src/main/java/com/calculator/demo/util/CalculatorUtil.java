f5316e90a7c84ab3da5c1f56aa64fb63e12f4ccc
yami <yaveranes@soaint.com>
2024-01-14 15:36:18 +0100

    adicionando utilitario para opraciones aritméticas

---
+++ src/main/java/com/calculator/demo/util/CalculatorUtil.java
@@ -0,0 +1,35 @@
+package com.calculator.demo.util;
+
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.math.BigDecimal;
+
+public class CalculatorUtil {
+
+    /**
+     * Adiciona dos números
+     * @param firstOperand primer operando
+     * @param secondOperand segundo operando
+     * @return suma de los números
+     */
+    public static Number add(String firstOperand, String secondOperand){
+        if( !(NumberUtils.isCreatable(firstOperand) && NumberUtils.isCreatable(secondOperand)))
+            throw new NumberFormatException("Number format not valid");
+
+        return new BigDecimal( firstOperand ).add( new BigDecimal(secondOperand ) );
+    }
+
+    /**
+     * Resta dos números
+     * @param firstOperand primer operando
+     * @param secondOperand segundo operando
+     * @return resta dos números
+     */
+    public static Number sub(String firstOperand, String secondOperand){
+        if( !(NumberUtils.isCreatable(firstOperand) && NumberUtils.isCreatable(secondOperand)))
+            throw new NumberFormatException("Number format not valid");
+
+        return new BigDecimal( firstOperand ).subtract( new BigDecimal(secondOperand ) );
+    }
+
+}
